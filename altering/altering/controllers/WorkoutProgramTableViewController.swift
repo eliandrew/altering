2a8859c69d819ad09953c50fef48e54c54df0451
Elias Andrew <elias.andrew@sap.com>
2024-07-06 13:30:16 -0700

    Started adding WorkoutPrograms

---
+++ altering/altering/controllers/WorkoutProgramTableViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class WorkoutProgramTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+}
